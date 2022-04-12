15ad700a573e1c42888db9eaac3851614b67cb99
Jose <josepls.v@hotmail.com>
2022-04-12 09:38:19 -0700

    first commit forked

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPAlindrome(string str)
+        {
+            /*var reversed = "";
+            if (str == null)
+            {
+                return false;
+            }
+            else
+            {
+                for(int i = str.Length - 1; i >= 0; i--)
+                {
+                    reversed += str.ToLower()[i];
+                }
+                return reversed == str.ToLower();
+            }*/
+            return (str == null) ? false : str.Replace(" ", "").ToLower().SequenceEqual(str.ToLower().Reverse()); ;
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,25 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hannah", true)]
+        [InlineData("levels", false)]
+        [InlineData("ciVIc", true)]
+        [InlineData("Nun", true)]
+        [InlineData("", true)]
+        [InlineData(null, false)]
+        [InlineData("Test", false)]
+        [InlineData("never odd or even", false)]
+        [InlineData("was it a cat I saw", false)]
+        public void PalindromeTester(string word, bool expected)
         {
-
+            //Arrange
+            var wordSmith = new WordSmith();
+            //Act
+            var actual = wordSmith.IsAPAlindrome(word);
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
